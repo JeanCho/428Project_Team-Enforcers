77cc69f1315c68c53b4bfd50dcd3d1573e826753
GitItBert <77079400+GitItBert@users.noreply.github.com>
2023-09-19 14:21:57 -0700

    setup login page for python,html and postgresql datbase using pgadmin4

---
+++ app/app.py
@@ -0,0 +1,49 @@
+from flask import Flask, render_template, request, redirect, url_for, session
+import psycopg2
+
+app = Flask(__name__)
+
+# Configure your PostgreSQL connection
+db_connection = psycopg2.connect(
+    database="428Project_Team-Enforcers",
+    user="postgres",
+    password="BAB0Y!",
+    host="localhost",
+    port="5432"
+)
+
+@app.route('/')
+def home():
+    return render_template('login.html')
+
+@app.route('/login', methods=['POST'])
+def login():
+    username = request.form['username']
+    password = request.form['password']
+    
+    cursor = db_connection.cursor()
+    cursor.execute("SELECT * FROM users WHERE username = %s AND password = %s", (username, password))
+    user = cursor.fetchone()
+    cursor.close()
+    
+    if user:
+        session['username'] = username
+        return redirect(url_for('dashboard'))
+    else:
+        return "Login failed. Please try again."
+
+@app.route('/dashboard')
+def dashboard():
+    if 'username' in session:
+        return f"Welcome, {session['username']}! This is your dashboard."
+    else:
+        return redirect(url_for('home'))
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('home'))
+
+if __name__ == '__main__':
+    app.secret_key = 'your_secret_key'
+    app.run(debug=True)
