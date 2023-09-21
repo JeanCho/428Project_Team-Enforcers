94d37c552f268741875d1cd06e374403b8dfc49b
Trevor-Wakefield <trevor.wakefield@wsu.edu>
2023-09-20 17:54:12 -0700

    tables are now succusfuly created

--- app/model/models.py
+++ app/model/models.py
@@ -14,44 +14,49 @@ def createTable():
    cursor = db_connection.cursor()
 
    ##sql = [] changing to this to clean up the exucutes later but for testing doing it 1 at a time
-   cursor.execite("DROP TABLE IF EXISTS USER")
+   cursor.execute("DROP TABLE IF EXISTS Account")
    # im sure password has some special rules but I don't know them yet
    # ill change it later.
    #
-   sql = '''CREATE TABLE USER(
-      USER_ID INT,
-      USERNAME CHAR(20) NOT NULL,
-      PASSWORD CHAR(20)
-   )'''
+   sql = """
+      CREATE TABLE Account(
+         USER_ID INT PRIMARY KEY,
+         USERNAME VARCHAR(20) NOT NULL,
+         PASSWORD VARCHAR(20)
+      )
+      """
 
-   cursor.execite(sql)
+   cursor.execute(sql)
 
-   cursor.execite("DROP TABLE IF EXISTS AUTHOR")
+   cursor.execute("DROP TABLE IF EXISTS AUTHOR")
    # implement relational database for a few of these
    # relation with the user who can act as an AUTHOR
-   sql = '''CREATE TABLE AUTHOR(
+   sql = '''CREATE TABLE AUTHOR (
       USER_ID INT, 
       ARTICLE_ID INT
-   )'''
+      );'''
 
-   cursor.execite(sql)
+   cursor.execute(sql)
 
-   cursor.execite("DROP TABLE IF EXISTS ARTICLE")
+   cursor.execute("DROP TABLE IF EXISTS ARTICLE")
    #Relation with who posted it
-   sql = '''CREATE TABLE ARTICLE(
+   sql = '''CREATE TABLE ARTICLE (
       ARTICLE_ID INT,
       USER_ID INT,
       ARTICLE CHAR(2500) 
-   )'''
+      );'''
 
-   cursor.execite(sql)
+   cursor.execute(sql)
 
-   cursor.execite("DROP TABLE IF EXISTS COMMIT")
+   cursor.execute("DROP TABLE IF EXISTS COMMIT")
    #Relation with who posted it and where it was posted
-   sql = '''CREATE TABLE COMMIT(
+   sql = '''CREATE TABLE COMMIT (
       USER_ID INT,
       COMMIT CHAR(500),
       ARTICLE_ID INT 
-   )'''
+      );'''
 
-   cursor.execite(sql)
+   cursor.execute(sql)
+   cursor.close()
+   db_connection.commit()
+   db_connection.close()
